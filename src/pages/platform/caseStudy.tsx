96d6ed61987574a91880f16aae678e3f502cdbc7
Amit <amitpandey0107@gmail.com>
2024-07-30 23:37:15 +0530

    Made responsive changes

--- src/pages/platform/caseStudy.tsx
+++ src/pages/platform/caseStudy.tsx
@@ -43,12 +43,12 @@ function MyVerticallyCenteredModal(props: any) {
                     </div>
                 </div>
                 <div className={`${styles.result} ${styles.mobile}`}>
-                            <h4>Result</h4>
-                            <div className={`${styles.resultBox}`}>
-                                <p>The implementation of Shodat eOps™ led to a significant improvement in the accuracy of battery life predictions, with accuracy rates close to 93%.</p>
-                                <p>Proactive alerting and predictive maintenance strategies resulted in a 25% reduction in maintenance costs by preventing unexpected battery failures.</p>
-                            </div>
-                        </div>
+                    <h4>Result</h4>
+                    <div className={`${styles.resultBox}`}>
+                        <p>The implementation of Shodat eOps™ led to a significant improvement in the accuracy of battery life predictions, with accuracy rates close to 93%.</p>
+                        <p>Proactive alerting and predictive maintenance strategies resulted in a 25% reduction in maintenance costs by preventing unexpected battery failures.</p>
+                    </div>
+                </div>
             </div>
         </Modal>
     );
@@ -70,7 +70,7 @@ export default function CaseStudy() {
                 <div className="row">
                     <div className="col-sm-12">
                         <div className={`${styles.caseStudySlider}`} id="caseStudySlider">
-                            <Carousel indicators={false}>
+                            <Carousel indicators={false} className={`${styles.desktopCaseSlider}`}>
 
                                 <Carousel.Item interval={700000}>
                                     <Stack
@@ -133,6 +133,89 @@ export default function CaseStudy() {
                                 </Carousel.Item>
 
                             </Carousel>
+
+                            <Carousel indicators={false} className={`${styles.mobileCaseSlider}`}>
+
+                                <Carousel.Item interval={700000}>
+                                    <Stack
+                                        direction="horizontal"
+                                        className="h-100 justify-content-center align-items-center"
+                                        gap={4}
+                                    >
+                                        <Card className={`${styles.sliderItem}`} onClick={() => setModalShow(true)}>
+                                            <div className={`${styles.csItem}`}>
+                                                <div className={`${styles.topItem}`}>
+                                                    <h4>EV charging stations</h4>
+                                                    <p>A prominent manufacturer of EV charging stations faced significant challenges in predicting battery life accurately. Inconsistent performance data, variations in usage patterns, and environmental factors made it difficult to provide reliable predictions.</p>
+                                                </div>
+                                                <div className={`${styles.imageItem}`}>
+                                                    <Image
+                                                        src="/img/platform/evCharging.png"
+                                                        alt="evCharging"
+                                                        height={290}
+                                                        width={392}
+                                                    />
+                                                    <div className={`${styles.purpleText}`}>To address these issues, the manufacturer implemented Shodat eOps™, an advanced asset tracking and quality management platform, to enhance their battery life prediction capabilities.</div>
+                                                </div>
+                                            </div>
+                                        </Card>
+                                    </Stack>
+                                </Carousel.Item>
+
+                                <Carousel.Item interval={700000}>                                    
+                                    <Stack
+                                        direction="horizontal"
+                                        className="h-100 justify-content-center align-items-center"
+                                        gap={4}
+                                    >
+                                        <Card className={`${styles.sliderItem}`}>
+                                            <div className={`${styles.csItem}`}>
+                                                <div className={`${styles.topItem}`}>
+                                                    <h4>Automotive manufacturer</h4>
+                                                    <p>A leading automotive manufacturer faced challenges in predicting tire wear accurately, which impacted vehicle safety, maintenance schedules, and overall operational efficiency. </p>
+                                                </div>
+                                                <div className={`${styles.imageItem}`}>
+                                                    <Image
+                                                        src="/img/platform/automotive.png"
+                                                        alt="automotive"
+                                                        height={290}
+                                                        width={392}
+                                                    />
+                                                    <div className={`${styles.purpleText}`}>By implementing Shodat eOps™, the manufacturer aimed to enhance their tire wear prediction capabilities. </div>
+                                                </div>
+                                            </div>
+                                        </Card>
+                                    </Stack>
+                                </Carousel.Item>
+
+                                <Carousel.Item interval={700000}>                                    
+                                    <Stack
+                                        direction="horizontal"
+                                        className="h-100 justify-content-center align-items-center"
+                                        gap={4}
+                                    >
+                                        <Card className={`${styles.sliderItem}`}>
+                                            <div className={`${styles.csItem}`}>
+                                                <div className={`${styles.topItem}`}>
+                                                    <h4>Manufacturing</h4>
+                                                    <p>A leader in the manufacturing industry needed an effective solution for detecting and addressing structural cracks in product components. Traditional inspection methods were labour-intensive, error-prone, and time-consuming.</p>
+                                                </div>
+                                                <div className={`${styles.imageItem}`}>
+                                                    <Image
+                                                        src="/img/platform/manufaturing.png"
+                                                        alt="manufaturing"
+                                                        height={290}
+                                                        width={392}
+                                                    />
+                                                    <div className={`${styles.purpleText}`}>To enhance their crack detection capabilities, the company implemented the Shodat eOps™platform.</div>
+                                                </div>
+                                            </div>
+                                        </Card>
+                                    </Stack>
+                                </Carousel.Item>
+                                
+                            </Carousel>
+
                         </div>
                     </div>
                 </div>
