fea34c5530617a6f26dd1e9e927984d872e18fef
Amit <amitpandey0107@gmail.com>
2024-08-24 23:35:24 +0530

    Data Engineering page - data storage section slider and text changes

--- src/pages/data-engineering/dataStorageSlider.tsx
+++ src/pages/data-engineering/dataStorageSlider.tsx
@@ -74,7 +74,7 @@ export default function DataStorageSlider(props: any) {
                             />
                         </div>
                         <div className={`${styles.itemBottom}`}>
-                            <h6>Handling Complex Analytical Calculations</h6>
+                            <h6>Data Warehousing</h6>
                             <p>We design and implement scalable data warehouses that provide a centralized repository for all your data needs and enable . Customized data marts are developed for specific business lines or departments, facilitating focused and efficient data analysis.</p>
                         </div>
                     </div>
@@ -89,8 +89,54 @@ export default function DataStorageSlider(props: any) {
                             />
                         </div>
                         <div className={`${styles.itemBottom}`}>
+                            <h6>Building Customized Data Models</h6>
+                            <p>Our team excels in creating tailored data models to suit your specific business requirements, ensuring data is organized, accessible, and optimized for performance. </p>
+                        </div>
+                    </div>        
+                    
+
+                    <div className={`${styles.featSlideItemBox}`}>
+                        <div className={`${styles.itemTop}`}>
+                            <Image
+                                height={168}
+                                width={160}
+                                alt="Ingesting Data from Various Sources"
+                                src="/img/dataEngineering/Data_Warehousing.svg"
+                            />
+                        </div>
+                        <div className={`${styles.itemBottom}`}>
                             <h6>Handling Complex Analytical Calculations</h6>
-                            <p>We design and implement scalable data warehouses that provide a centralized repository for all your data needs and enable . Customized data marts are developed for specific business lines or departments, facilitating focused and efficient data analysis.</p>
+                            <p>We utilize sophisticated data transformation techniques  to prepare data for complex analyses. Our approach ensures that data is cleansed, normalised, and structured efficiently, which is crucial for accurate analytical computations. </p>
+                        </div>
+                    </div>
+
+                    <div className={`${styles.featSlideItemBox}`}>
+                        <div className={`${styles.itemTop}`}>
+                            <Image
+                                height={168}
+                                width={160}
+                                alt="Ingesting Data from Various Sources"
+                                src="/img/dataEngineering/Data_Warehousing.svg"
+                            />
+                        </div>
+                        <div className={`${styles.itemBottom}`}>
+                            <h6>Building Visualization Layers</h6>
+                            <p>We transform raw data into intuitive dashboards and reports, making complex data easily understandable. Our team also builds efficient views for report consumption, ensuring that data is presented in a user-friendly manner.</p>
+                        </div>
+                    </div>
+
+                    <div className={`${styles.featSlideItemBox}`}>
+                        <div className={`${styles.itemTop}`}>
+                            <Image
+                                height={168}
+                                width={160}
+                                alt="Ingesting Data from Various Sources"
+                                src="/img/dataEngineering/Data_Warehousing.svg"
+                            />
+                        </div>
+                        <div className={`${styles.itemBottom}`}>
+                            <h6>File Based Storage</h6>
+                            <p>We offer the ability to store, extract and process data stored in various file formats such as CSV, XLSX, PDFs, JPEGs, and more, ensuring all your data sources are integrated and managed efficiently.</p>
                         </div>
                     </div>
 
