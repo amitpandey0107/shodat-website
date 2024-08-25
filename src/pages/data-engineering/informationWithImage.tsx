3583aeb54b7dad70bd9cabc30610e3229db03ef2
Amit <amitpandey0107@gmail.com>
2024-08-25 23:37:13 +0530

    Data Engineering page - Images changes & desogn and responsive changes

--- src/pages/data-engineering/informationWithImage.tsx
+++ src/pages/data-engineering/informationWithImage.tsx
@@ -19,10 +19,10 @@ export default function InformationWithImage() {
                         <div className="col-sm-12 col-md-6 col-lg-6">
                             <div className={`${styles.imageBlock}`}>
                                 <Image
-                                    src="/img/dataEngineering/application_integration_bg.png"
+                                    src="/img/dataEngineering/Visualisation_Layers.png"
                                     alt="Visualisation Layers and dashboarding capabilities"
-                                    height={523}
-                                    width={608}
+                                    height={364}
+                                    width={532}
                                 />
                             </div>
                         </div>
@@ -33,26 +33,37 @@ export default function InformationWithImage() {
             <div className={`${styles.rightTextLeftImage}`}>
                 <div className="container-fluid">
                     <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-6">
+                        <div className={`col-sm-12 col-md-6 col-lg-6 ${styles.bgPink}`}>
                             <div className={`${styles.imageBlock}`}>
                                 <Image
-                                    src="/img/dataEngineering/application_integration_bg.png"
-                                    alt="Visualisation Layers and dashboarding capabilities"
-                                    height={523}
-                                    width={608}
+                                    src="/img/dataEngineering/ML_Engines.png"
+                                    alt="ML Engines Models and consulting"
+                                    height={616}
+                                    width={690}
                                 />
                             </div>
                         </div>
-                        <div className="col-sm-12 col-md-6 col-lg-6">
+                        <div className={`col-sm-12 col-md-6 col-lg-6 ${styles.bgPink}`}>
                             <div className={`${styles.infoBlock}`}>
-                                <h2>ML Engines Models and <br />consulting</h2>
-                                <p>The data pipeline extends into our Machine Learning (ML) capabilities for further value enrichment.</p>
+                                <h2>ML Engines Models and consulting</h2>
+                                <p>The data pipeline extends into our Machine Learning (ML) capabilities for further value enrichment. </p>
                                 <p>Processed data is cleansed and a layer of feature engineering is applied before feeding it into our model training and testing frameworks.</p>
                                 <p>Anomaly detection, predictive analytics, forecasting, document extraction, image processing and automated report generation functions are designed to empower businesses with crucial insights to drive innovation, efficiency, and gain a competitive edge. </p>
                                 <p>Our comprehensive suite of ML services and autonomous turn key pipelines ensures that your organisation can leverage the full potential of data to achieve your strategic objectives.</p>
+                                <div className={`${styles.btnWrapper}`}>
+                                    <button className={`${styles.btnBlack}`}>
+                                        <span>Read more</span>
+                                        <Image
+                                            src="/img/dataEngineering/arrow-right.png"
+                                            alt="Teradata"
+                                            height={24}
+                                            width={24}
+                                        />
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
