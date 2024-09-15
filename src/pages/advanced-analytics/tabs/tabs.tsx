d717ac8560353e86cdc59ec8894b5f1f8eed5443
amitpandey0107 <amitpandey0107@gmail.com>
2024-09-15 23:45:06 +0530

    Modified Tab section on advanced alalytics page for mobile slider

--- src/pages/advanced-analytics/tabs/tabs.tsx
+++ src/pages/advanced-analytics/tabs/tabs.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from './tabs.module.css';
 import Image from "next/image";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useSearchParams } from 'next/navigation'
 
 import AdvancedDataModeling from "./advancedDataModeling";
 import AIMachineLearningIntegration from "./aiMachineLearningIntegration";
@@ -11,6 +15,64 @@ import PerformanceOptimization from "./performanceOptimization";
 
 export default function Tabs() {
     const [tab, setTab] = useState(1);
+    const [oldSlide, setOldSlide] = useState(0);
+    const [activeSlide, setActiveSlide] = useState(0);
+    const [activeSlide2, setActiveSlide2] = useState(0);
+    const [toggleButton, setToggleButton] = useState(false);
+    const searchParams = useSearchParams();
+    const sliderID = searchParams?.get('id')
+    const settings = {
+        dots: false,
+        arrows:false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        beforeChange: (current: any, next: any) => {
+            setOldSlide(current);
+            setActiveSlide(next);
+        },
+        afterChange: (current: any) => setActiveSlide2(current),
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    arrows:true,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows:true,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                    arrows:true,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows:true,
+                }
+            }
+        ]
+    };
+
+    useEffect(() => {
+        activeSlide > 0 ? setToggleButton(true) : setToggleButton(false);
+    }, [activeSlide])
 
     const handleTabFunction = (tabID: number) => {
         setTab(tabID)
@@ -32,6 +94,54 @@ export default function Tabs() {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-sm-12">
+                            <div className={`advancedAnalyticsSlider ${styles.advancedAnalyticsSlider} ${styles.tabwrap}`}>
+                                <Slider {...settings}>
+                                    <div className={`${styles.btnwrap}`}>
+                                        <button
+                                            className={`${styles.btnTab} ${tab === 1 ? `${styles.active}` : ``} `}
+                                            onClick={() => handleTabFunction(1)}
+                                        >
+                                            Advanced Data Modeling</button>
+                                    </div>
+                                    <div className={`${styles.btnwrap}`}>
+                                        <button
+                                            className={`${styles.btnTab} ${tab === 2 ? `${styles.active}` : ``} `}
+                                            onClick={() => handleTabFunction(2)}
+                                        >
+                                            Performance Optimization</button>
+                                    </div>
+                                    <div className={`${styles.btnwrap}`}>
+                                        <button
+                                            className={`${styles.btnTab} ${tab === 3 ? `${styles.active}` : ``} `}
+                                            onClick={() => handleTabFunction(3)}
+                                        >
+                                            Enhanced Data Connectivity</button>
+                                    </div>
+                                    <div className={`${styles.btnwrap}`}>
+                                        <button
+                                            className={`${styles.btnTab} ${tab === 4 ? `${styles.active}` : ``} `}
+                                            onClick={() => handleTabFunction(4)}
+                                        >
+                                            Enterprise-Level BI</button>
+                                    </div>
+                                    <div className={`${styles.btnwrap}`}>
+                                        <button
+                                            className={`${styles.btnTab} ${tab === 5 ? `${styles.active}` : ``} `}
+                                            onClick={() => handleTabFunction(5)}
+                                        >
+                                            Custom Visuals and Reporting</button>
+                                    </div>
+                                    <div className={`${styles.btnwrap}`}>
+                                        <button
+                                            className={`${styles.btnTab} ${tab === 6 ? `${styles.active}` : ``} `}
+                                            onClick={() => handleTabFunction(6)}
+                                        >
+                                            AI and Machine Learning Integration</button>
+                                    </div>
+                                </Slider>
+                            </div>
+                        </div>
+                        {/* <div className="col-sm-12">
                             <div className={`${styles.tabwrap}`}>
                                 <div className={`${styles.btnwrap}`}>
                                     <button
@@ -76,7 +186,7 @@ export default function Tabs() {
                                         AI and Machine Learning Integration</button>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
 
@@ -85,12 +195,12 @@ export default function Tabs() {
                         <div className="row">
                             <div className="col-sm-12 col-md-6 col-lg-6">
                                 <div className={`${styles.leftContent}`}>
-                                    {tab===1 && <AdvancedDataModeling />}
-                                    {tab===2 && <PerformanceOptimization />}
-                                    {tab===3 && <EnhancedDataConnectivity />}
-                                    {tab===4 && <EnterpriseLevelBI />}
-                                    {tab===5 && <CustomVisualsReporting />}
-                                    {tab===6 && <AIMachineLearningIntegration />}
+                                    {tab === 1 && <AdvancedDataModeling />}
+                                    {tab === 2 && <PerformanceOptimization />}
+                                    {tab === 3 && <EnhancedDataConnectivity />}
+                                    {tab === 4 && <EnterpriseLevelBI />}
+                                    {tab === 5 && <CustomVisualsReporting />}
+                                    {tab === 6 && <AIMachineLearningIntegration />}
                                 </div>
                             </div>
                             <div className="col-sm-12 col-md-6 col-lg-6">
